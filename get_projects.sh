9fe3e5c7c381734845e098a502dd180150c636fc
paste-project <83464728+paste-project@users.noreply.github.com>
2021-05-02 01:03:44 -0300

    Update get_projects.sh

--- get_projects.sh
+++ get_projects.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+pip3 install gdown
+gdown https://drive.google.com/uc?id=1TU3cW8S30RwOoDKtpzeI9SO8E3FxmJFa
 
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1TU3cW8S30RwOoDKtpzeI9SO8E3FxmJFa' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1TU3cW8S30RwOoDKtpzeI9SO8E3FxmJFa" -O projects.tar.gz && rm -rf /tmp/cookies.txt
 
-tar -xvzf projects.tar.gz
+tar -xvzf projects.tar.gz > /dev/null
 rm -f projects.tar.gz
